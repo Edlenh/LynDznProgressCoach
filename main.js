607dd0dcc34bd75383277ea7de21dcfc2d6b7337
Edward <herreraed93@gmail.com>
2023-01-25 16:54:19 -0800

    add scrolling text

--- main.js
+++ main.js
@@ -22,3 +22,38 @@ const slider = new A11YSlider(document.querySelector('.slider'), {
       }
   });
 
+'use strict';
+var pause = 2;
+var speed = 2;
+var texts = ['feel stuck and confused about life?' ,
+ 'feel dissatisfied with where you are in life?',
+ 'desire change in your life ?',
+];
+(function() {
+  var text = texts[0];
+  var cur = 0,
+    dir = 1,
+    cur_text = 0;
+
+  var s = 0;
+
+  setInterval(function loop() {
+    cur += dir;
+    if (cur < 0) {
+      cur = 0;
+      dir = -dir;
+      cur_text = (++cur_text) % 3;
+      text = texts[cur_text];
+    }
+    if (cur > text.length) {
+      cur = text.length;
+      if (s++ > pause) {
+        s = 0
+        dir = -dir;
+      }
+    }
+    document.querySelector("#animation").setAttribute('class', text.toLowerCase())
+    document.querySelector("#animation").innerHTML = text.substring(0, cur);
+  }, 100 / speed)
+
+})()
